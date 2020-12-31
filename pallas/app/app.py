e291a9a930298327e223ab4fe343c61a20a28698
Ramya Iyer <ramya.iyer@wpengine.com>
2020-12-31 13:38:43 -0600

    Initial commit of dockerized server

---
+++ pallas/app/app.py
@@ -0,0 +1,64 @@
+import os
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+import logging
+
+application = Flask(__name__)
+gunicorn_logger = logging.getLogger('gunicorn.error')
+application.logger.handlers = gunicorn_logger.handlers
+
+application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+
+mongo = PyMongo(application)
+db = mongo.db
+
+@application.route('/')
+def index():
+    application.logger.info("indexing")
+    return jsonify(
+        status=True,
+        message='Welcome to the Dockerized Flask MongoDB app: 6'
+    )
+
+@application.route('/todo')
+def todo():
+    application.logger.info("todoing")
+    try:
+        _todos = db.todo.find()
+        item = {}
+        data = []
+        for todo in _todos:
+            item = {
+                'id': str(todo['_id']),
+                'todo': todo['todo']
+            }
+            data.append(item)
+        return jsonify(
+            status=True,
+            data=data
+        )
+    except Exception as err:
+        application.logger.error("error: ", err)
+        return jsonify(
+            status=False,
+            message=str(err)
+        )
+
+
+@application.route('/todo', methods=['POST'])
+def createTodo():
+    data = request.get_json(force=True)
+    item = {
+        'todo': data['todo']
+    }
+    db.todo.insert_one(item)
+
+    return jsonify(
+        status=True,
+        message='To-do saved successfully!'
+    ), 201
+
+if __name__ == "__main__":
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
