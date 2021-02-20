03c12dff02ed0d233858fa83d9e4591adefd8bd5
Ramya Iyer <ramya.iyer@wpengine.com>
2021-02-20 15:41:22 -0600

    Harden API schema and implement DB read/write
    Update callers of the API to conform to the updated schema. Fix some
    bugs in calendar sync.

--- pallas/app/resolvers/__init__.py
+++ pallas/app/resolvers/__init__.py
@@ -1,5 +1,6 @@
 from ariadne import make_executable_schema, load_schema_from_path, QueryType, MutationType
-from ariadne import snake_case_fallback_resolvers, convert_kwargs_to_snake_case
+from datetime import datetime
+from .db_bridge import DbBridge
 import logging
 import time
 
@@ -8,13 +9,30 @@ logger = logging.getLogger('gunicorn.error.' + __name__)
 
 
 type_defs = load_schema_from_path('schema.graphql')
-
-
 query = QueryType()
+mutation = MutationType()
 
-@convert_kwargs_to_snake_case
+db_bridge = DbBridge.get_instance()
+
+
+@mutation.field('addToSchedule')
+def resolve_add_to_schedule(_, info, showTimes):
+    return db_bridge.add_to_schedule(showTimes)
+
+@query.field('latestSchedule')
+def resolve_latest_schedule(_, info):
+    return db_bridge.get_latest_schedule(10)
+
 @query.field('nowPlaying')
 def resolve_now_playing(_, info):
+    now_playing = db_bridge.get_now_playing(datetime.now())
+    if not now_playing:
+        return {
+            "__typename": "NothingPlaying",
+            "tryAgainInMin": 0.5
+        }
+
+    last_played_episode = db_bridge.get_last_played(now_playing['name'])
     anything_on = "video"
 
     if (anything_on == "video"):
@@ -23,7 +41,7 @@ def resolve_now_playing(_, info):
             "uri": "Key and Peele/Season 1/Episode 1",
             "name": "Key and Peele",
             "url": "http://192.168.1.22:8000/nick.mp4",
-            "last_played_epoch": int(time.time())
+            "lastPlayed": datetime.now().isoformat()
         }
     elif (anything_on == "audio"):
         return {
@@ -31,27 +49,11 @@ def resolve_now_playing(_, info):
             "uri": "Piper Jones/The Wandering Stars/Gordon Duncan Tunes",
             "name": "Piper Jones",
             "url": "http://192.168.1.22:8000/piper.mp3",
-            "last_played_epoch": int(time.time())
-        }
-    else:
-        return {
-            "__typename": "NothingPlaying",
-            "try_again_in_min": 0.5
+            "lastPlayed": datetime.now().isoformat()
         }
 
-
-mutation = MutationType()
-
-@convert_kwargs_to_snake_case
 @mutation.field('markPlayed')
-def resolve_mark_played(_, info, uri):
-    return True
+def resolve_mark_played(_, info, name, uri):
+    return db_bridge.mark_played(name, uri, datetime.now())
 
-@convert_kwargs_to_snake_case
-@mutation.field('addToSchedule')
-def resolve_add_to_schedule(_, info, showTimes):
-    logger.info(f"Adding to schedule: {showTimes}")
-    return True
-
-
-schema = make_executable_schema(type_defs, query, mutation, snake_case_fallback_resolvers)
+schema = make_executable_schema(type_defs, query, mutation)
---
+++ pallas/app/resolvers/db_bridge.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+import logging
+import os
+from pymongo import MongoClient
+import time
+from types import SimpleNamespace
+
+
+logger = logging.getLogger('gunicorn.error.' + __name__)
+
+
+class DbBridge:
+    @staticmethod
+    def get_instance():
+        mongo_uri = f"mongodb://{os.environ['MONGODB_HOSTNAME']}:27017"
+        client = MongoClient(mongo_uri)
+        db = client[os.environ['MONGODB_DATABASE']]
+        return DbBridge(db)
+
+    def __init__(self, db):
+        self.db = db
+
+    def add_to_schedule(self, show_times):
+        show_time_entries = [
+            {
+                "name": entry['name'],
+                "start_time_epoch": self._datetime_str_to_epoch(entry['showStart']),
+                "end_time_epoch": self._datetime_str_to_epoch(entry['showEnd']),
+                "write_time_epoch": self._datetime_to_epoch(datetime.now())
+            }
+            for entry in show_times
+        ]
+        self.db.schedule.insert_many(show_time_entries)
+        return True
+
+    def get_latest_schedule(self, max_num_shows: int):
+        show_time_entries = self.db.schedule.find().sort("start_time_epoch", -1).limit(max_num_shows)
+        show_times = [
+            {
+                "name": show_time['name'],
+                "showStart": self._datetime_epoch_to_str(show_time['start_time_epoch']),
+                "showEnd": self._datetime_epoch_to_str(show_time['end_time_epoch'])
+            }
+            for show_time in show_time_entries
+        ]
+        return show_times
+
+    def get_now_playing(self, now: datetime):
+        now_epoch = self._datetime_to_epoch(now)
+
+        now_playing = self.db.schedule.find_one(
+            {
+                "start_time_epoch": { "$lte": now_epoch },
+                "end_time_epoch": { "$gte": now_epoch }
+            }
+        )
+        if (now_playing):
+            return {
+                "name": now_playing['name'],
+                "showStart": self._datetime_epoch_to_str(now_playing['start_time_epoch']),
+                "showEnd": self._datetime_epoch_to_str(now_playing['end_time_epoch'])
+            }
+        return None
+
+    def mark_played(self, name: str, uri: str, played_time: datetime):
+        entry = {
+            "name": name,
+            "uri": uri,
+            "played_time_epoch": self._datetime_to_epoch(played_time),
+            "write_time_epoch": self._datetime_to_epoch(datetime.now())
+        }
+        self.db.history.insert_one(entry)
+        return True
+
+    def get_last_played(self, name):
+        episode_entry = self.db.history.find_one(sort=[("played_time_epoch", -1)])
+        if (episode_entry):
+            return {
+                "name": episode_entry['name'],
+                "uri": episode_entry['uri'],
+                "lastPlayed": self._datetime_epoch_to_str(episode_entry['played_time_epoch'])
+            }
+        return None
+
+    @classmethod
+    def _datetime_to_epoch(cls, datetime_obj):
+        return int(datetime_obj.timestamp())
+
+    @classmethod
+    def _datetime_str_to_epoch(cls, datetime_str):
+        return cls._datetime_to_epoch(datetime.fromisoformat(datetime_str))
+
+    @classmethod
+    def _datetime_epoch_to_str(cls, epoch):
+        return datetime.fromtimestamp(epoch).isoformat()
